47fadcf7bebba5b03edd25d2bcef431dccf67420
Josh Robinson <hexorx@gmail.com>
2023-03-22 00:35:24 -1000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+import core from '@actions/core'
+import axios from 'axios'
+import { Commit } from '@crtxio/hddata'
+
+async function run() {
+  try {
+    // Get input values from action.yml
+    const publisher = core.getInput('publisher')
+    const topic = core.getInput('topic')
+    const key = core.getInput('key')
+    const ptr = core.getInput('ptr')
+
+    const commit = await Commit.sign({ ptr }, key, topic)
+    const { iss } = commit.json
+
+    await axios.post(publisher + '/commits', commit.encoded)
+
+    // Set the output value
+    core.setOutput('issuer', iss)
+    core.setOutput('commit', commit.encoded)
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run()
